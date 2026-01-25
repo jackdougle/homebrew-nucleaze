9fac9cc884fdae40adbd7d1ce5e62c14c698476a
Jack Douglass <102892299+jackdougle@users.noreply.github.com>
2026-01-25 01:00:57 -0700

    Update nucleaze to version 1.4.2

--- Formula/nucleaze.rb
+++ Formula/nucleaze.rb
@@ -1,8 +1,8 @@
 class Nucleaze < Formula
   desc "Fast Rust DNA read filtering tool inspired by BBDuk"
   homepage "https://github.com/jackdougle/nucleaze"
-  url "https://github.com/jackdougle/nucleaze/archive/refs/tags/v1.4.1.tar.gz"
-  sha256 "7403f8e018cf684d876dd4300c496919a8e77b978b1e503f0534483dbdb14d9f"
+  url "https://github.com/jackdougle/nucleaze/releases/tag/v1.4.2"
+  sha256 "279f1c94db56e4e18e86ca9f6219faf137f2f9d1543886d911a344e926b49ffa"
   license "MIT"
 
   depends_on "rust" => :build
